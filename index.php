9254ecdbd357db49d657714ccc602978cd87c652
fenris22 <galexred22@gmail.com>
2023-12-08 20:44:05 -0600

    switching to php

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
     <title>Mountain Fortress</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-    <body align="center">
+    <body>
         <header>Mountain Fortress</header>
         <header><a href="https://discord.gg/comfy-factory-433039858794233858">Discord</a></header>
         <p>Feel free to contribute to the site: <a href="https://github.com/fenris22/MFWebsite">GitHub</a></p>
+        <p>Main Train Blueprints: <a href="maintrain.html">Main</a></p>
     </body>
 </html>
---
+++ maintrain.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Main Train Blueprints</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>Main Train Blueprints</header>
+</body>
+</html>
